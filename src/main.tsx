cc32297f4549eea077724edbf69166708ea645f4
Jangtastic <ayi.hardiyanto@dkatalis.com>
2026-06-03 08:41:35 +0700

    feat: wrap application in QueryClientProvider for React Query integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { queryClient } from './lib/queryClient';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <QueryClientProvider client={queryClient}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </QueryClientProvider>
   </StrictMode>,
 );
